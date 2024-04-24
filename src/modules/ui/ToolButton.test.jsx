02f1019af0e1682ed2167494a6f0f5a6e53beb48
Tom Simmons <tomasimmons@gmail.com>
2024-04-24 00:07:39 -0400

    add working jest tests

---
+++ src/modules/ui/ToolButton.test.jsx
@@ -0,0 +1,36 @@
+import ToolButton from './ToolButton.jsx'
+import renderer from 'react-test-renderer'
+import { cleanup, fireEvent, render } from '@testing-library/react'
+
+
+it('Uses active styling when active', () => {
+  const component = renderer.create(
+    <ToolButton active={true} onClick={() => {}} />
+  )
+
+  let tree = component.toJSON()
+  expect(tree).toMatchSnapshot()
+})
+
+it('Does not use active styling when inactive', () => {
+  const component = renderer.create(
+    <ToolButton active={false} onClick={() => {}} />
+  )
+
+  let tree = component.toJSON()
+  expect(tree).toMatchSnapshot()
+})
+
+it('Triggers onClick callback when clicked', () => {
+  const mockOnClick = jest.fn()
+  const {queryByText, getByText} = render(
+    <ToolButton active={false} onClick={mockOnClick}>
+      Label
+    </ToolButton>
+  )
+
+  expect(queryByText(/Label/i)).toBeTruthy()
+  fireEvent.click(getByText(/Label/i))
+
+  expect(mockOnClick.mock.instances.length).toBe(1)
+})
